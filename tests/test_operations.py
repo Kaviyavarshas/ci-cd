c76447feb23b51e97a7ca5f8708eb848f39311bc
kaviyavarsha <kaviyavarshas01@gmail.com>
2026-07-24 16:14:02 +0530

    Fix flake8 style issues

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,7 +1,10 @@
-from src.math_operations import add,subtract
+
+from src.math_operations import add, subtract
+
 
 def test_add():
-    assert add(2,3)==5
+    assert add(2, 3) == 5
+
 
 def test_subtract():
-    assert subtract(5,3)==2 
+    assert subtract(5, 3) == 2
